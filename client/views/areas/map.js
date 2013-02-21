6eac23342c46ef588caeab64f6b060cf6ac16493
Derek Marcinyshyn <derek@monasheemountainmultimedia.com>
2013-02-20 19:25:42 -0800

    add weather and cloud layers to map

--- client/views/areas/map.js
+++ client/views/areas/map.js
@@ -5,6 +5,17 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     var map = new google.maps.Map($("#map_canvas")[0], mapOptions);
+
+    var weatherLayer = new google.maps.weather.WeatherLayer({
+        temperatureUnits: google.maps.weather.TemperatureUnit.CELSUIS,
+        windSpeedUnits: google.maps.weather.WindSpeedUnit.KILOMETERS_PER_HOUR
+    });
+    weatherLayer.setMap(map);
+
+    var cloudLayer = new google.maps.weather.CloudLayer();
+    cloudLayer.setMap(map);
+
+
 }
 
 Template.map.rendered = function(){
