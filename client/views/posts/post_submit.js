cb04dc67ced5c85c9c38a60de2a095175d3279ad
Derek Marcinyshyn <derek@monasheemountainmultimedia.com>
2013-02-21 13:18:36 -0800

    add google map to post_submit with lat/long

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -3,11 +3,45 @@ Template.post_submit.rendered = function() {
     $('#time').timepicker();
     $('#setTimeButton').on('click', function() {
         $('#time').timepicker('setTime', new Date());
-
     });
+
+    // add map
+    submitMapInitialize();
+
+
 }
 
 
 Template.post_submit.events = {
+
+}
+
+function submitMapInitialize() {
+    var submitMapOptions = {
+        center: new google.maps.LatLng(50.999497, -118.196411),
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        panControl: false,
+        streetViewControl: false
+    };
+    var submitMap = new google.maps.Map($("#submit_map")[0], submitMapOptions);
+
+    google.maps.event.addListener(submitMap, 'click', function(event){
+        google.maps.event.clearListeners(submitMap,'click');
+
+        var submitMarker = new google.maps.Marker({
+            position: event.latLng,
+            draggable: true
+        });
+
+        submitMarker.setMap(submitMap);
+
+        google.maps.event.addListener( submitMarker, 'mouseup', function() {
+            var latlong = submitMarker.getPosition();
+            $('#latlong').val(latlong);
+        });
+
+    });
+
 
 }
