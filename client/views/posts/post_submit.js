96e9d9b0aa83a1737564ba51dfe9c24dfa814518
Derek Marcinyshyn <derek@monasheemountainmultimedia.com>
2013-02-21 22:16:57 -0800

    add comments add required fields and validation

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -9,15 +9,83 @@ Template.post_submit.rendered = function() {
     submitMapInitialize();
 
     // show avalanche observations
-    $('#show_avi').click(function() {
-        $('#see_avi').toggle(this.checked);
+    $('#showAvi').click(function() {
+        $('#seeAvi').slideToggle(this.checked);
     });
 }
 
-
 Template.post_submit.events = {
 
-}
+    'click input[type=submit]':function(e, instance) {
+        e.preventDefault();
+
+        $(e.target).addClass('disabled');
+
+        var isSubmitFormValid = true;
+
+        $('.required').each(function() {
+            if ($.trim($(this).val()).length == 0) {
+                // do not post
+                isSubmitFormValid = false;
+                $(this).addClass('highlight');
+                $(e.target).removeClass('disabled');
+                //$('.submitMessage').show();
+                $('.submitMessage').show().html('Sorry you have required fields.');
+
+            } else {
+                // post to database
+                $(this).removeClass('highlight');
+
+
+            }
+        });
+
+        if (isSubmitFormValid) {
+            // grab the variables from the post_submit form
+            var location = $('#location').val();
+            var latLng = $('#latLng').val();
+            var date = $('#date').val();
+            var time = $('#time').val();
+            var temp = $('#temp').val();
+            var tempMeasured = $('input:radio[name=tempMeasure]:checked').val();
+            var wind = $('select.wind option:selected').val();
+            var windSpeed = $('select.windSpeed option:selected').val();
+            var snowSurface = $('select.snowSurface option:selected').val();
+            var footPen = $('select.footPen option:selected').val();
+            var newSnow = $('select.newSnow option:selected').val();
+            var aviCause = $('select.aviCause option:selected').val();
+            var aviAmount = $('select.aviAmount option:selected').val();
+            var aviSize = $('select.aviSize option:selected').val();
+            var aviAspect = $('select.aviAspect option:selected').val();
+            var aviElevation = $('select.aviElevation option:selected').val();
+            var aviDepth = $('select.aviDepth option:selected').val();
+            var comments = $('#comments').val();
+
+            var properties = {
+                location:       location,
+                latLng:         latLng,
+                date:           date,
+                time:           time,
+                temp:           temp,
+                tempMeasured:   tempMeasured,
+                wind:           wind,
+                windSpeed:      windSpeed,
+                snowSurface:    snowSurface,
+                footPen:        footPen,
+                newSnow:        newSnow,
+                aviCause:       aviCause,
+                aviAmount:      aviAmount,
+                aviSize:        aviSize,
+                aviAspect:      aviAspect,
+                aviElevation:   aviElevation,
+                aviDepth:       aviDepth,
+                comments:       comments
+            };
+
+            console.log(properties);
+        }
+    }
+};
 
 function submitMapInitialize() {
     var submitMapOptions = {
@@ -32,7 +100,7 @@ function submitMapInitialize() {
     google.maps.event.addListener(submitMap, 'click', function(event){
         google.maps.event.clearListeners(submitMap,'click');
 
-        $('#latlong').val(event.latLng);
+        $('#latLng').val(event.latLng);
 
         var submitMarker = new google.maps.Marker({
             position: event.latLng,
@@ -42,11 +110,8 @@ function submitMapInitialize() {
         submitMarker.setMap(submitMap);
 
         google.maps.event.addListener( submitMarker, 'mouseup', function() {
-            var latlong = submitMarker.getPosition();
-            $('#latlong').val(latlong);
+            var latLng = submitMarker.getPosition();
+            $('#latLng').val(latLng);
         });
-
     });
-
-
 }
